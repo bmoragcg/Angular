cece36e9fe2df39df3b13a2a6377d8cc23c857f3
bmoragcg <bmora@garantiascomunitarias.com>
2021-05-03 11:33:59 -0500

    Prueba #10 para subir proyecto al repositorio

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 "use strict";
 (() => {
     const enviarMision = (xmen) => {
-        console.log(`Enviado a ${xmen.nombre} a la mision`);
+        console.log(`Enviado a ${xmen.nombre} a la mision con el poder ${xmen.poder}`);
     };
     const regresarAlCuartel = (xmen) => {
-        console.log(`Regresando a ${xmen.nombre} al cuartel`);
+        console.log(`Regresando a ${xmen.nombre} al cuartel con la edad de ${xmen.edad} años`);
     };
     const wolverine = {
         nombre: 'logan',
-        edad: 21
+        edad: 21,
+        poder: 'Volar'
     };
     enviarMision(wolverine);
     regresarAlCuartel(wolverine);
--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -5,21 +5,21 @@
         nombre:string;
         edad:number;
         poder?:string;
-    
     }
 
     const enviarMision = (xmen:Xmen) => {
-        console.log(`Enviado a ${xmen.nombre} a la mision`);
+        console.log(`Enviado a ${xmen.nombre} a la mision con el poder ${xmen.poder}`);
     }
 
     const regresarAlCuartel = (xmen:Xmen) => {
-        console.log(`Regresando a ${xmen.nombre} al cuartel`);
+        console.log(`Regresando a ${xmen.nombre} al cuartel con la edad de ${xmen.edad}`);
     }
  
 
     const wolverine: Xmen  = {
         nombre: 'logan',
-        edad: 21
+        edad: 21,
+        poder:'Volar'
     }
 
     enviarMision(wolverine);
